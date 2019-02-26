54d4de61591447069b1afc99735f53e1b0a77aaf
Torpus <torpus@hotmail.com>
2019-02-26 00:46:31 -0600

    welp, complete refactor was needed...

--- ffmpeg-compare.sh
+++ ffmpeg-compare.sh
@@ -2,73 +2,77 @@
 
 SOURCE_FILE=$1
 SIZE_MULTIPLIER=${2:-1}
-echo Input file: $SOURCE_FILE
-echo Target output size: $SIZE_MULTIPLIER
-PREV_FILE=""
+echo Input file: "$SOURCE_FILE"
+echo Target output size: "$SIZE_MULTIPLIER"
+
+PRESETS=( ultrafast veryslow )
+CRFS=( 0 16 24)
+TUNES=( grain film )
+
 PREV_VMAF=0
-PRESETS=( ultrafast superfast veryfast faster fast medium slow slower veryslow )
-CRFS=( 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 )
-TUNES=( grain film animation )
-TEMP_FILE_LIST=temp_file_list.txt
+
 BASE_FILENAME=$(basename "$SOURCE_FILE" | cut -f 1 -d '.')
 BASE_FILENAME=${BASE_FILENAME// /_}
 BASE_DIR="$BASE_FILENAME"-tests
-TEMP_DIR="$BASE_DIR"/source_temp
-mkdir -p "$TEMP_DIR"
+REF_DIR="$BASE_DIR"/reference
+ENC_DIR="$BASE_DIR"/encoded
+mkdir -p "$REF_DIR" "$ENC_DIR"
 
 grabSnippet() {
     START_OFFSET=${1:-0}
     OFFSET_ARRAY=" $OFFSET_ARRAY $START_OFFSET "
-    if [ "$(($START_OFFSET + 3))" -lt "$ORIGINAL_DURATION" ]
+    if [ "$((START_OFFSET + 15))" -lt "$ORIGINAL_DURATION" ]
     then
-        grabSnippet "$(($START_OFFSET + 60))"
+        grabSnippet "$((START_OFFSET + 300))"
     fi
 }
-
 updateBest() {
-    PREV_FILE="$BASE_DIR"/"$THIS_FILE"
     PREV_FILESIZE="$THIS_FILESIZE"
-    PREV_VMAF="$VMAF"
+    PREV_VMAF="$THIS_VMAF"
     BEST_PRESET="$PRESET"
     BEST_TUNE="$TUNE"
     BEST_CRF="$CRF"
-    echo New best preset="$BEST_PRESET", tune="$BEST_TUNE", crf="$BEST_CRF" with vmaf="$VMAF"
+    echo New best preset="$BEST_PRESET", tune="$BEST_TUNE", crf="$BEST_CRF" with vmaf="$THIS_VMAF"
 }
-
 bytesToHuman () {
     numfmt --to=iec-i --format='%.5f' "$1"
 }
-
-flessThanPercent() {
+floatLessThanPercent() {
     awk -v n1="$1" -v n2="$2" -v n3="$3" 'BEGIN {if (n1+0<((n2+0)*(n3+0))) exit 0; exit 1}'
 }
-
-flessthan() {
+floatLessThan() {
     awk -v n1="$1" -v n2="$2" 'BEGIN {if (n1+0<n2+0) exit 0; exit 1}'
 }
-
-fequal() {
+floatEquals() {
     awk -v n1="$1" -v n2="$2" 'BEGIN {if (n1+0==n2+0) exit 0; exit 1}'
 }
+runEncode() {
+    BASE_FILENAME="$1"
+    TEMP_NAME=${BASE_FILENAME//reference/encoded}
+    ffmpeg -loglevel panic -i "$BASE_FILENAME" -c:v libx264 -crf "$CRF" -preset "$PRESET" -tune "$TUNE" -sn -an "$TEMP_NAME"
+}
+runVmaf() {
+    BASE_FILENAME="$1"
+    TEMP_NAME=${BASE_FILENAME//reference/encoded}
+    TEMP_VMAF=$(ffmpeg -i "$TEMP_NAME" -i "$BASE_FILENAME" -lavfi libvmaf="pool=perc5:log_fmt=json:model_path=model/vmaf_4k_v0.6.1.pkl" -f null - 2>&1 | grep "\[libvmaf" | grep "VMAF score" | grep -Poh "([0-9]{1,3}\.[0-9]{1,15})")
+    VMAF=" $VMAF $TEMP_VMAF "
+}
 
-echo Starting processing "$SOURCE_FILE"
-ORIGINAL_DURATION=$(ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "$SOURCE_FILE" | grep -Poh -m 1 "([0-9]{1,9})" | head -1)
+ORIGINAL_DURATION=$(ffprobe -v panic -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "$SOURCE_FILE" | grep -Poh -m 1 "([0-9]{1,9})" | head -1)
 
 grabSnippet
 OFFSET_ARRAY=( $OFFSET_ARRAY )
 
 for i in "${!OFFSET_ARRAY[@]}"
 do
-    TEMP_FILE="$BASE_FILENAME"_source_"$i".mkv
-    echo "file '$TEMP_FILE'" >> "$TEMP_DIR"/"$TEMP_FILE_LIST"
-    ffmpeg -loglevel panic -ss "${OFFSET_ARRAY[$i]}" -noaccurate_seek -i "$SOURCE_FILE" -t "00:00:03" -c:v copy -avoid_negative_ts 1 -sn -an "$TEMP_DIR"/"$TEMP_FILE"
+    REF_FILE="$BASE_FILENAME"_reference_"$i".mkv
+    ffmpeg -loglevel panic -ss "${OFFSET_ARRAY[$i]}" -i "$SOURCE_FILE" -t "00:00:15" -c:v copy -avoid_negative_ts 1 -sn -an "$REF_DIR"/"$REF_FILE"
 done
-REF_FILE="$BASE_DIR"/"$BASE_FILENAME"_source.mkv
-ffmpeg -loglevel panic -f concat -i "$TEMP_DIR"/"$TEMP_FILE_LIST" -c:v copy -an -sn "$REF_FILE"
-rm -rf "$TEMP_DIR"
-REF_FILESIZE=$(stat -c%s "$REF_FILE")
+
+
+REF_FILESIZE=$(du "$REF_DIR" | grep -Poh -m 1 "([0-9]{1,999})(?=\s)")
 PREV_FILESIZE="$REF_FILESIZE"
-echo Created composite video with filesize "$(bytesToHuman "$REF_FILESIZE")"
+echo Created sample set of videos with total filesize of "$(bytesToHuman "$REF_FILESIZE")"
 
 BEST_PRESET=""
 BEST_TUNE=""
@@ -80,41 +84,49 @@ do
     do
         for CRF in "${CRFS[@]}"
         do
-            echo Running with preset="$PRESET", crf="$CRF", and tune="$TUNE"
-            THIS_FILE="$BASE_FILENAME"_"$PRESET"_crf"$CRF"_"$TUNE"
-            ffmpeg -loglevel panic -i "$REF_FILE" -c:v libx264 -crf "$CRF" -preset "$PRESET" -tune "$TUNE" -sn -an "$BASE_DIR"/"$THIS_FILE".mkv
-            THIS_FILESIZE=$(stat -c%s "$BASE_DIR"/"$THIS_FILE".mkv)
-            if flessThanPercent "$THIS_FILESIZE" "$REF_FILESIZE" "$SIZE_MULTIPLIER"
+            echo Running preset="$PRESET", crf="$CRF", and tune="$TUNE"
+            for REF_FILE in $REF_DIR/*
+            do
+                runEncode "$REF_FILE"
+            done
+            THIS_FILESIZE=$(du "$ENC_DIR" | grep -Poh -m 1 "([0-9]{1,999})(?=\s)")
+            if floatLessThanPercent "$THIS_FILESIZE" "$REF_FILESIZE" "$SIZE_MULTIPLIER"
             then
                 echo Continuing with VMAF: 0"$(bc <<< "scale=5; $THIS_FILESIZE / $REF_FILESIZE")"X file size
-                VMAF=$(ffmpeg -i "$BASE_DIR"/"$THIS_FILE".mkv -i "$REF_FILE" -lavfi libvmaf="pool=perc5:log_fmt=json:model_path=model/vmaf_4k_v0.6.1.pkl" -f null - 2>&1 | grep "\[libvmaf" | grep "VMAF score" | grep -Poh "([0-9]{1,3}\.[0-9]{1,15})")
-                if flessthan "$PREV_VMAF" "$VMAF"
+                for REF_FILE in $REF_DIR/*
+                do
+                    runVmaf "$REF_FILE"
+                done
+                VMAF=( $VMAF )
+                VMAF_SUM=$( IFS="+"; bc <<< "${VMAF[*]}" )
+                THIS_VMAF=$(bc <<< "scale=15; $VMAF_SUM / ${#VMAF[@]}")
+                echo "$THIS_VMAF"
+                if floatLessThan "$PREV_VMAF" "$THIS_VMAF"
                 then
                     updateBest
-                    if [ "$PREV_FILE" != "" ]
-                    then
-                        rm "$PREV_FILE".*
-                    fi
-                elif fequal "$PREV_VMAF" "$VMAF"
+                elif floatEquals "$PREV_VMAF" "$THIS_VMAF"
                 then
                     if [ "$THIS_FILESIZE" -lt "$PREV_FILESIZE" ]
                     then
-                        echo Same quality result: vmaf="$VMAF" but smaller file size.
+                        echo Same quality result: vmaf="$THIS_VMAF" but smaller file size.
                         updateBest
                     else
-                        echo Same quality result: vmaf="$VMAF" but larger or same file size.  Retaining previous best preset="$BEST_PRESET", tune="$BEST_TUNE", crf="$BEST_CRF"
+                        echo Same quality result: vmaf="$THIS_VMAF" but larger or same file size.  Retaining previous best preset="$BEST_PRESET", tune="$BEST_TUNE", crf="$BEST_CRF"
                     fi
                 else
-                    echo Lower quality result: vmaf="$VMAF" \< "$PREV_VMAF".  Retaining previous best preset="$BEST_PRESET", tune="$BEST_TUNE", crf="$BEST_CRF"
-                    rm "$BASE_DIR"/"$THIS_FILE".*
+                    echo Lower quality result: vmaf="$THIS_VMAF" \< "$PREV_VMAF".  Retaining previous best preset="$BEST_PRESET", tune="$BEST_TUNE", crf="$BEST_CRF"
+                    rm "$ENC_DIR"/*
                     break
                 fi
             else
                 echo Too big: "$(bc <<< "scale=2; $THIS_FILESIZE / $REF_FILESIZE")"X file size
-                rm "$BASE_DIR"/"$THIS_FILE".mkv
             fi
+            rm "$ENC_DIR"/*
         done
     done
 done
-
+rm -rf "$BASE_DIR"
 echo best preset="$BEST_PRESET", best tune="$BEST_TUNE", best crf="$BEST_CRF"
+echo Use command:   ffmpeg -loglevel panic -i "$SOURCE_FILE" -c:v libx264 -crf "$BEST_CRF" -preset "$BEST_PRESET" -tune "$BEST_TUNE" -sn -an out/"$(basename "$SOURCE_FILE")"
+
+exit 0
