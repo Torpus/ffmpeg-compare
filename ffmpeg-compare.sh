6cfacbad3760f1e2770a1fa96f6ea97acc6505c6
Torpus <torpus@hotmail.com>
2019-02-26 00:50:46 -0600

    welp, complete refactor was needed...

--- ffmpeg-compare.sh
+++ ffmpeg-compare.sh
@@ -5,9 +5,9 @@ SIZE_MULTIPLIER=${2:-1}
 echo Input file: "$SOURCE_FILE"
 echo Target output size: "$SIZE_MULTIPLIER"
 
-PRESETS=( ultrafast veryslow )
-CRFS=( 0 16 24)
-TUNES=( grain film )
+PRESETS=( ultrafast superfast veryfast faster fast medium slow slower veryslow )
+CRFS=( 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 )
+TUNES=( grain film animation )
 
 PREV_VMAF=0
 
